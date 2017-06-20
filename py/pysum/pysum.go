f76ca2f0e50f5422be73216cb6490aa044842301
Wang Cong <ousatoru@hotmail.com>
2017-06-20 17:21:19 +0800

    pymodule

--- py/pysum/pysum.go
+++ py/pysum/pysum.go
@@ -1,16 +1,37 @@
 package main
 
-// #cgo pkg-config: python
-// #include <Python.h>
+/*
+#cgo pkg-config: python
+#include <Python.h>
+
+int PyArg_ParseTuple_LL(PyObject *, long long *, long long *);
+PyObject * sum(PyObject *, PyObject *);
+*/
 import "C"
 
-//export Sum
-func Sum(self, args *C.PyObject) *C.PyObject {
-	// var a, b C.longlong
-	// if C.PyArg_ParseTuple_LL(args, &a, &b) == 0 {
-	// 	return nil
-	// }
-	return C.PyLong_FromLongLong(0)
+//export sum
+func sum(self, args *C.PyObject) *C.PyObject {
+	var a, b C.longlong
+	if C.PyArg_ParseTuple_LL(args, &a, &b) == 0 {
+		return nil
+	}
+	return C.PyLong_FromLongLong(a + b)
+}
+
+//export initpys
+func initpys() {
+	C.Py_InitModule4(C.CString("pys"), &sumMethods[0], C.CString("doc"),
+		nil, C.PYTHON_API_VERSION)
+}
+
+var sumMethods = []C.PyMethodDef{
+	{
+		C.CString("sum"),
+		C.PyCFunction(C.sum),
+		C.METH_VARARGS, [4]byte{},
+		C.CString("Add two numbers."),
+	},
+	{nil, nil, 0, [4]byte{}, nil},
 }
 
 func main() {
