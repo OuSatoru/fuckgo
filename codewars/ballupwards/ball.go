e0a618ccb863c285ffd520dccf4467599b0b61ed
Wang Cong <ousatoru@hotmail.com>
2017-07-10 22:15:53 +0800

    ball

---
+++ codewars/ballupwards/ball.go
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/ball-upwards/train/go
+package ballupwards
+
+func MaxBall(v0 int) int {
+	return v0
+}
+
+func km2ms(v float64) float64 {
+	return v / 3.6
+}
+
+func ms2km(v float64) float64 {
+	return v * 3.6
+}
