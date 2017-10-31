549cbb544b24c143aad52b46a22f646c7ff3fe2b
Wang Cong <ousatoru@hotmail.com>
2017-10-31 11:13:56 +0800

    if have first must read?

--- socket/server/main.go
+++ socket/server/main.go
@@ -23,10 +23,10 @@ func main() {
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// buf := make([]byte, 1024)
+	// _, err := conn.Read(buf)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	conn.Write([]byte("傻逼"))
 }
