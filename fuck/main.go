b447fc2ed1f5f4386062e1667debc894249bf22f
Wang Cong <ousatoru@hotmail.com>
2017-11-02 08:16:44 +0800

    test binary

--- fuck/main.go
+++ fuck/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 )
@@ -9,4 +11,13 @@ func main() {
 	str := "0000WA"
 	sh := hex.EncodeToString([]byte(str))
 	fmt.Println(sh)
+	buf := new(bytes.Buffer)
+	var data = []int8{2, 3}
+	for _, v := range data {
+		err := binary.Write(buf, binary.LittleEndian, v)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Printf("%v\n", buf.Bytes())
 }
