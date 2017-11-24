6658062789d256e51313d3b05c7e26780924371f
Wang Cong <ousatoru@hotmail.com>
2017-11-24 14:15:09 +0800

    test jwt

--- restjwt/main.go
+++ restjwt/main.go
@@ -1,12 +1,153 @@
 package main
 
-import "net/http"
+import (
+	"crypto/rsa"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 
-func main() {
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"github.com/urfave/negroni"
+)
+
+const (
+	privKeyPath = "app.rsa"
+	pubKeyPath  = "app.rsa.pub"
+)
+
+var (
+	verifyKey *rsa.PublicKey
+	signKey   *rsa.PrivateKey
+)
+
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+type Response struct {
+	Data string `json:"data"`
+}
+
+func initKeys() {
+	signBytes, err := ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startServer() {
 	http.HandleFunc("/login", loginHandler)
+	http.Handle("/resource", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(ProtectedHandler)),
+	))
 	http.ListenAndServe(":2333", nil)
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func main() {
+	initKeys()
+	startServer()
+}
 
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{"Gained access to protected resource"}
+	JsonResponse(response, w)
+}
+
+func ValidateTokenMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+		func(token *jwt.Token) (interface{}, error) {
+			return verifyKey, nil
+		})
+	if err != nil {
+		if token.Valid {
+			next(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Token is not valid")
+		}
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Unauthorized access to this resource")
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var user UserCredentials
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, "Error in request")
+		return
+	}
+
+	if strings.ToLower(user.Username) != "someone" {
+		if user.Password != "p@ssword" {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Println("Error logging in")
+			fmt.Fprint(w, "Invalid credentials")
+			return
+		}
+	}
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+	claims["iat"] = time.Now().Unix()
+	token.Claims = claims
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error extracting the key")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	tokenString, err := token.SignedString(signKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error while signing the token")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	response := Token{tokenString}
+	JsonResponse(response, w)
+}
+
+func JsonResponse(response interface{}, w http.ResponseWriter) {
+	j, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application-json")
+	w.Write(j)
 }
