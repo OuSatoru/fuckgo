e9550a4f66e9db0edfe30b9bc6f08bc9416eaff5
Wang Cong <ousatoru@hotmail.com>
2018-02-03 09:15:59 +0800

    bl

---
+++ cron/expbl/expbl.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		<-ticker.C
+		if exists(fmt.Sprintf("/fr/data/xms/%s.bl", yesterday())) {
+			continue
+		}
+		_, err := exec.Command("db2", "connect to jsbods user ods using ods@98").Output()
+		if err != nil {
+			log.Println(err)
+		}
+		out, err := exec.Command("db2", "SELECT * FROM REPORT.ODS_HXZZYEB WHERE SJRQ = to_char(CURRENT_DATE - 1 DAY, 'yyyymmdd') AND KMH = '91310000' FETCH FIRST 1 ROW ONLY").Output()
+		if err != nil {
+			log.Println(err)
+		}
+		// fmt.Printf("%s\n", out)
+		if !strings.Contains(string(out), "0 record(s) selected") {
+			out, err := exec.Command("./expbl.sh").Output()
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Printf("%s\n", out)
+		}
+
+	}
+
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		// os.IsNotExist(err)
+		return false
+	}
+	return true
+}
+
+func yesterday() string {
+	now := time.Now()
+	return now.AddDate(0, 0, -1).Format("20060102")
+}
