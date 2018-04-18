0ca77ad24b2280f41c2d1ee421e2e97d70351091
Wang Cong <ousatoru@hotmail.com>
2018-04-18 10:55:05 +0800

    10w

--- fuck/main.go
+++ fuck/main.go
@@ -10,7 +10,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	now := time.Now()
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < 100000; i++ {
 		go func() {
 			wg.Add(1)
 			time.Sleep(5 * time.Second)
