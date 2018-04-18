1e0db1509434bd3679befd20d7c9f7a2b23e8f2e
Wang Cong <ousatoru@hotmail.com>
2018-04-18 09:22:48 +0800

    test ip

--- fuck/main.go
+++ fuck/main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"strings"
+	"net/http"
 )
 
 func main() {
-	_, err := exec.Command("db2", "connect to jsbods user ods using ods@98").Output()
-	if err != nil {
-		log.Println(err)
-	}
-	out, err := exec.Command("db2", "SELECT * FROM REPORT.ODS_ZBTJB WHERE SJRQ = to_char(CURRENT_DATE - 1 DAY, 'YYYYMMDD') AND ZBDH = '0001'").Output()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(string(out))
-	if !strings.Contains(string(out), "0 record(s) selected") {
-		out, err := exec.Command("./exp.sh").Output()
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Printf("%s\n", out)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s - %v", r.RemoteAddr, r.Header)
+	})
+	http.ListenAndServe(":2333", nil)
 }
