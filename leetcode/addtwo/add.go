c971719758e769b85cf281da3a9c08108b692455
Wang Cong <ousatoru@hotmail.com>
2018-09-14 09:50:57 +0800

    2

--- leetcode/addtwo/add.go
+++ leetcode/addtwo/add.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -8,23 +12,28 @@ type ListNode struct {
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	carry := 0
 	ret := new(ListNode)
-	ret.Val = 0
-	for l1 != nil && l2 != nil {
-		v1 := 0
-		v2 := 0
+	tmp := ret
+	for l1 != nil || l2 != nil || carry > 0 {
+		s := carry
 		if l1 != nil {
-			v1 = l1.Val
+			s += l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
-			v2 = l2.Val
+			s += l2.Val
 			l2 = l2.Next
 		}
-		carry = (v1 + v2 + carry) / 10
-		value := (v1 + v2 + carry) % 10
+		carry = s / 10
+		value := s % 10
+		tmp.Next = &ListNode{Val: value}
+		tmp = tmp.Next
 	}
+	return ret.Next
 }
 
 func main() {
-
+	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
+	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
+	res := addTwoNumbers(l1, l2)
+	fmt.Println(*res)
 }
